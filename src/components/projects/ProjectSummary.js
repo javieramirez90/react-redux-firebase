0fb477d7a04d236e2dfd2f7861c61548d13a36e1
javieramirez90 <javier.ramirez@aiesec.net>
2019-02-09 22:36:34 -0600

    Project summary

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+  return(
+    <div className="card x-depth-0 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Project Title</span>
+        <p>Posted by Xavier Ramírez</p>
+        <p className="grey-text">8th february, 9 am</p>
+      </div>
+    </div>    
+  )
+}
+
+
+export default ProjectSummary;
